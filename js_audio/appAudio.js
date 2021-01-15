624832837bb5866a498aee752c7ec18b01b0b2a7
firewarrah <katya86_pin@mail.ru>
2021-01-15 23:19:35 +0200

    Update appAudio.js
    Messages on error added for valid string that's not found in api response

--- js_audio/appAudio.js
+++ js_audio/appAudio.js
@@ -1,17 +1,38 @@
 import {addTracks} from "./audioManager.js"
 
 window.onload = () => {
+  
   init();
 }
 
 const init = () => {
-  let myUrl = "https://deezerdevs-deezer.p.rapidapi.com/search?rapidapi-key=0553c29e4bmsh3a239d5a07dbdb4p1e9ae1jsnc9399dacde13&q=queen"
-  //doApi(myUrl);
-  viewEvents();
+  //let myUrl = "https://deezerdevs-deezer.p.rapidapi.com/search?rapidapi-key=0553c29e4bmsh3a239d5a07dbdb4p1e9ae1jsnc9399dacde13&q=queen"
+ // doApi(myUrl);
+ viewEvents();
 }
 
-
-const addEvents = (input, button) =>{
+function validateInput(input) {
+  let hasError = false;
+  const searchReg = /^[a-zA-Z0-9-]+$/;
+  document.getElementById("id_row").innerHTML = "";
+ console.log(!searchReg.test(input.value));
+  if(input.value == ""){
+    document.querySelector("#error").innerHTML = "Please enter a search term";
+    hasError = true;
+  } else if (!searchReg.test(input.value)){
+    document.querySelector("#error").innerHTML = "Special symbols are not allowed, try again";
+    hasError = true;
+  } else {
+    
+    return true;
+  }
+  if(hasError == true){
+    console.log("error true");
+    return false;
+  } 
+}
+const addEvents = (input, button) => {
+  
   // Execute a function when the user releases a key on the keyboard
   input.addEventListener("keydown", function(event) {
   // Number 13 is the "Enter" key on the keyboard
@@ -25,8 +46,16 @@ const addEvents = (input, button) =>{
   // מאזין ללחיצה על הכפתור שנמצא בבאדי של החיפוש
   button.addEventListener("click",() => {
     let valSearch = input.value;
-    let myUrl = `https://deezerdevs-deezer.p.rapidapi.com/search?rapidapi-key=0553c29e4bmsh3a239d5a07dbdb4p1e9ae1jsnc9399dacde13&q=${valSearch}`
-    doApi(myUrl);
+    let valid = validateInput(input);
+    if(valid === true){
+      console.log("Valid");
+      document.querySelector("#error").innerHTML = "";
+      let myUrl = `https://deezerdevs-deezer.p.rapidapi.com/search?rapidapi-key=0553c29e4bmsh3a239d5a07dbdb4p1e9ae1jsnc9399dacde13&q=${valSearch}`
+      doApi(myUrl);
+    } else {
+      console.log("Not valid");
+    }
+    
   });
 
 }
@@ -42,11 +71,15 @@ const viewEvents = () => {
 const doApi = (_url) => {
   return new Promise((resolve, reject) => {
   fetch(_url)
-  .then(resp => resp.json())
+  .then((resp) => resp.json())
   .then(data => {
-    //console.log(data.hits);
-    addTracks(data.data)
-    //TODO : funciton from manger of pixa
+    let checkIfEmpty = Object.keys(data.data).length === 0 && data.data.value.constructor === Object;
+    if(!checkIfEmpty){
+         addTracks(data.data)  
+    }
+    else {
+      document.querySelector("#error").innerHTML = "Please enter valid value and try again";
+    }
   })
   .catch(err =>{
     reject(err)
